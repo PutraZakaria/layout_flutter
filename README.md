4d0758b514066debc8069571229c2d884cc078fd
PutraZakaria <p1578003@gmail.com>
2024-10-09 18:35:38 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Pemrograman Mobile Minggu 6
+
+## Nama: Putra Zakaria Muzaki
+## Kelas: TI-3H / 19
+## NIM: 2241720220
+
+<br>
+
+### Praktikum 1
+Pada praktikum ini membuat bagian judul menggunakan widget Container, Row, dan Column, serta menambahkan teks dan ikon
+![alt](img/P1.png)
+
+### Praktikum 2
+Pada praktikum ini dilakukan proses membuat baris tombol yang berisi ikon dan teks menggunakan metode _buildButtonColumn() untuk mengatur warna dan konten setiap kolom.
+![alt](img/P2.png)
+
+### Praktikum 3
+Pada praktikum ini dilakukan proses menambahkan bagian teks deskriptif dengan padding, yang menjelaskan gambar
+![alt](img/P3.png)
+
+### Praktikum 4
+Di praktikum ini dilakukan proses menyiapkan gambar dengan menambahkannya ke folder images, mengonfigurasikannya di pubspec.yaml, dan menampilkannya dalam layout menggunakan ListView agar mendukung scrolling pada perangkat dengan resolusi lebih kecil
+![alt](img/P4.png)
---
+++ lib/main.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    Widget titleSection = Container(
+      padding: const EdgeInsets.all(32),
+      child: Row(
+        children: [
+          Expanded(
+            /* soal 1*/
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                /* soal 2*/
+                Container(
+                  padding: const EdgeInsets.only(bottom: 8),
+                  child: const Text(
+                    'Wisata Gunung di Batu',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Text(
+                  'Batu, Malang, Indonesia',
+                  style: TextStyle(color: Colors.grey),
+                ),
+              ],
+            ),
+          ),
+          /* soal 3*/
+          Icon(
+            Icons.star,
+            color: Colors.red,
+          ),
+          const Text('41'),
+        ],
+      ),
+    );
+
+    Color color = Theme.of(context).primaryColor;
+
+    Widget buttonSection = Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        _buildButtonColumn(color, Icons.call, 'CALL'),
+        _buildButtonColumn(color, Icons.near_me, 'ROUTE'),
+        _buildButtonColumn(color, Icons.share, 'SHARE'),
+      ],
+    );
+
+    Widget textSection = Container(
+      padding: const EdgeInsets.all(32),
+      child: const Text(
+        'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese Alps. Situated 1,578 meters above sea level, it is one of the larger Alpine Lakes. A gondola ride from Kandersteg, followed by a half-hour walk through pastures and pine forest, leads you to the lake, which warms to 20 degrees Celsius in the summer. Activities enjoyed here include rowing, and riding the summer toboggan run.\n'
+        'Putra Zakaria Muzaki - 2241720220',
+        softWrap: true,
+      ),
+    );
+
+    return MaterialApp(
+      title: 'Putra Zakaria Muzaki - 2241720220',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Flutter layout demo'),
+        ),
+        body: ListView(
+          children: [
+            Image.asset('images/lake.jpg',
+                width: 600, height: 240, fit: BoxFit.cover),
+            titleSection,
+            buttonSection,
+            textSection,
+          ],
+        ),
+      ),
+    );
+  }
+
+  Column _buildButtonColumn(Color color, IconData icon, String label) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(icon, color: color),
+        Container(
+          margin: const EdgeInsets.only(top: 8),
+          child: Text(
+            label,
+            style: TextStyle(
+              fontSize: 12,
+              fontWeight: FontWeight.w400,
+              color: color,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
